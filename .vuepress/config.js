4789368ead10ad83c723198f451dc6465b1b6863
xwzbupt <xwze@bupt.cn>
2022-05-14 19:38:57 +0800

    s

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -23,8 +23,17 @@ module.exports = {
         ["meta", {name: "keywords", content: "Java 全栈知识体系, java体系, java知识体系, java框架,java详解,java学习路线,java spring, java面试, 知识体系, java技术体系, java编程, java编程指南,java开发体系, java开发,java教程,java,java数据结构, 算法, 开发基础"}],
         ["meta", {name: "apple-mobile-web-app-capable", content: "yes"}]
     ],
-    plugins: [
-    ],
+    plugins: {
+        '@vuepress/back-to-top': true,
+        '@vuepress/medium-zoom': {
+            selector: 'img.zoom-custom-imgs',
+            // medium-zoom options here
+            // See: https://github.com/francoischalifour/medium-zoom#options
+            options: {
+                margin: 16
+            }
+        }
+    },
     themeConfig: {
         docsRepo: "realpdai/tech-arch-doc",
         editLinks: true,
